28a14d4516da7d82ea34fdee9190b2fdefdd50f5
AME <abduelamin@hotmail.co.uk>
2024-01-20 01:24:58 +0000

    little code refactoring to make more readable. Plus I added global variables to the 4 solar parameters so that we can easily access their values

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-// my notes to remind me on my learning
-
-// fetch does not care about errors
-// fetch only rejects the promise if there is a network error i.e. there is no connectivity.
-
-// To truly handle errors, fetch won't do it for us. We have to manually step in a do this. This is important.
-
-// Basically we want to look at the response object to see if it worked i.e. make sure we got a decent return
-
-// Action - need to look at how to stop the API key being visible.
-
-// Fetch response objective provides a property called ok.
 
 // solar potential, max array panel count number, mac sunshine hours per year. carbon offset per year,
+
 // calculation (panel count of 8, each panel entry killowatt enregy value, pull that for panel and get total. The api gives number of panels associated with a house and gives each value of KW enregy each panel obtains) solar potnetila = number of panels. 
 
 let longitudeDetails;
 let latitudeDetails;
 let ukPostCode = ``;
+let maxSunshineHoursPerYear;
+let carbonOffsetFactorKgPerMwh;
+let panelNumber;
+let yearlyEnergyDcKwh;
 
 const geo_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=${ukPostCode}&key=AIzaSyBSz3w7EQnyHPXu2qDA4hz71uCVntYBug8`
 
@@ -24,16 +17,6 @@ const SolarQueryURL = `https://solar.googleapis.com/v1/buildingInsights:findClos
 
 let googleMapsURL = `https://maps.googleapis.com/maps/api/js?AIzaSyBSz3w7EQnyHPXu2qDA4hz71uCVntYBug8&libraries=geometry&callback=initMap`
 
-
-// This is a map which we could place on pageload once the user enters their location. But its not fully ready yet, but an idea to have if you want to include it. Could also help with solar mapping aspect. This seems long winded to incorporate so unless we need it I'll remove it. 
-
-// const mainMap = () => {
-//   const map = new google.maps.Map(document.getElementById('map'), {
-
-//   });
-
-
-// } 
 
 
 // Function for creating elements to make my life easier for when I have to make multiple elements
@@ -142,15 +125,17 @@ const fetchSolarInfo = (lat, lon) => {
       
       console.log(data)
 
-      let maxSunshineHoursPerYear = data.solarPotential.maxSunshineHoursPerYear
+       maxSunshineHoursPerYear = data.solarPotential.maxSunshineHoursPerYear
 
-      let carbonOffsetFactorKgPerMwh = data.solarPotential.carbonOffsetFactorKgPerMwh
+       carbonOffsetFactorKgPerMwh = data.solarPotential.carbonOffsetFactorKgPerMwh
 
-      let panelNumber = data.solarPotential.solarPanelConfigs[0].panelsCount
+       panelNumber = data.solarPotential.solarPanelConfigs[0].panelsCount
 
-      let yearlyEnergyDcKwh = data.solarPotential.solarPanelConfigs[0].yearlyEnergyDcKwh
+       yearlyEnergyDcKwh = data.solarPotential.solarPanelConfigs[0].yearlyEnergyDcKwh
 
       console.log(maxSunshineHoursPerYear, carbonOffsetFactorKgPerMwh, panelNumber, yearlyEnergyDcKwh);
+
+      return [maxSunshineHoursPerYear, carbonOffsetFactorKgPerMwh, panelNumber, yearlyEnergyDcKwh]
       
     })
 
@@ -162,6 +147,7 @@ const fetchSolarInfo = (lat, lon) => {
 function openModal() {
   document.getElementById('errorMessage').innerText = "We're sorry, but our product doesn't currently cover your living area. We're in beta testing and plan to expand our coverage in the future.";
   document.getElementById('errorModal').style.display = 'block';
+  // Need to add some styling to it 
 }
 
 // Function to close the modal
@@ -177,32 +163,6 @@ modalCloseBtn.addEventListener(`click`, () => {
 
 
 }
-
-
-
-
-
-
-// const getGeoLoc = async () => {
-//   try {
-//     const response = await fetch(geo_URL);
-//     // throw own error manually and then catch error below
-//     if (!response.ok) {
-//       throw new Error(`http error! Status: ${response.status}`);
-//     }
-//     const data = await response.json();
-//     //console.log(data);
-//     //console.log(data.results[0].geometry.location.lat);
-//     //console.log(data.results[0].geometry.location.lng);
-//     longitudeDetails = data.results[0].geometry.location.lng;
-//     latitudeDetails = data.results[0].geometry.location.lat;
-//     console.log(`Lat details: ${latitudeDetails}`);
-//     console.log(`Long details: ${longitudeDetails}`);
-//   } catch (error) {
-//     console.log("something is wrong");
-//     console.log(`Error: ${error}`);
-//   }
-// };
 
 
 
